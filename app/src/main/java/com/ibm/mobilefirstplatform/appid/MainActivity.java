b688884cbff313e2ea04b6ed423c05951c7cea46
Anton Aleksandrov <antona@us.ibm.com>
2017-01-20 11:21:04 -0600

    fixing case sensitive file name

--- app/src/main/java/com/ibm/mobilefirstplatform/appid/MainActivity.java
+++ app/src/main/java/com/ibm/mobilefirstplatform/appid/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
