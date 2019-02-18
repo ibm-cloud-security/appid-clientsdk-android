7b4075bf2cd43d9363114b89e05cd7fe8ad1737f
Anup Rokkam <arokkamp@us.ibm.com>
2019-02-18 15:27:06 -0600

    typo

--- app/src/main/java/com/ibm/mobilefirstplatform/appid/MainActivity.java
+++ app/src/main/java/com/ibm/mobilefirstplatform/appid/MainActivity.java
@@ -35,8 +35,8 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String tenantId = "39a37f57-a227-4bfe-a044-93b6e6060b61";
-    private final static String region = AppID.REGION_UK_STAGE1; //AppID.REGION_UK ,AppID.REGION_SYDNEY
+    private final static String tenantId = "AppID_tenantId";;
+    private final static String region = AppID.REGION_US_SOUTH; //AppID.REGION_UK ,AppID.REGION_SYDNEY
     private final static String protectedUrl = "protected_URL";
 
     private final static Logger logger = Logger.getLogger(MainActivity.class.getName());
